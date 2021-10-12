16269e7a07a4991e9ba6c1d41d19300eb412a886
dmontoya1600 <78572569+dmontoya1600@users.noreply.github.com>
2021-10-12 11:41:28 -0500

    created the routes, but still need to implement the logic for replacing the entries

--- backend/routes/api/embedded.js
+++ backend/routes/api/embedded.js
@@ -5,7 +5,7 @@ const {singlePublicFileUpload, singleMulterUpload, deleteFile} = require('../../
 
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Array, Image } = require('../../db/models');
+const { User, Array, Image, Embeddeded } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
@@ -15,14 +15,14 @@ router.get(
     asyncHandler(async (req, res) => {
       const location = req.params.location;
 
-      const allImages = await Image.findAll({
+      const allEmbedded = await Embeddeded.findAll({
         where: {location: location}
       })
 
 
       const array = []
-      allImages.forEach(imageObj => {
-        array.push(imageObj.dataValues)
+      allEmbedded.forEach(embeddedObj => {
+        array.push(embeddedObj.dataValues)
       })
 
       return res.json({
@@ -36,22 +36,24 @@ router.get(
     '/',
     asyncHandler(async (req, res) => {
       const { location } = req.body;
-      const {imageUrl, Key} = await singlePublicFileUpload(req.file);
 
-      const image = await Image.create({
+        // CODE HASN'T BEEN IMPLEMENTED YET, BUT IT SHOULD CHECK FOR AN ENTRY IN THE POSITION
+        // IF THERE IS AN ENTRY THEN IT'LL REPLACE IT, ELSE JUST CREATE A NEW ENTRY
+
+      const embedded = await Embeddeded.create({
         location,
-        imageUrl,
-        key: Key
+        position,
+        code,
       })
 
-      const allImages = await Image.findAll({
+      const allEmbedded = await Embeddeded.findAll({
         where: {location: location}
       })
 
 
       const array = []
-      allImages.forEach(imageObj => {
-        array.push(imageObj.dataValues)
+      allEmbedded.forEach(embeddedObj => {
+        array.push(embeddedObj.dataValues)
       })
 
       return res.json({
