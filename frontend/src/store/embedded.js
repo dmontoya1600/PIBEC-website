c84e8140d4ef78f59f17df58a617953e47a99a1f
dmontoya1600 <78572569+dmontoya1600@users.noreply.github.com>
2021-10-14 18:20:37 -0500

    fixed directioning of homepage content (css)

--- frontend/src/store/embedded.js
+++ frontend/src/store/embedded.js
@@ -1,80 +1,80 @@
-import { csrfFetch } from './csrf';
+// import { csrfFetch } from './csrf';
 
-export const ADD_IMAGE ='images/addImage';
+// export const ADD_IMAGE ='images/addImage';
 
-const setImages = (location, array) => {
-  return {
-    type: ADD_IMAGE,
-    array,
-    location,
-  };
-};
-
-
-export const getEmbeddedCode = (location, position = 0) => async dispatch => {
-  const response = await csrfFetch(`api/embedded/${location}`)
-  const data = await response.json()
-  dispatch(setEmbeddedCode(location, data.array))
-  /*
-    RESPONSE SHOULD RETURN AN ARRAY OF ALL THE EMBEDDED CODE FROM THAT LOCATION
-    RESPONSE SHOULD BE AN ARRAY OF EMBEDDED CODE WITH THEIR POSITION AND LOCATION
-
-  */
-}
-
-export const createUpdateCode = (location, position = 0) => async dispatch => {
-  const response = await csrfFetch(`api/embedded/${location}`)
-  const data = await response.json()
-
-  dispatch(setEmbeddedCode(location, data.array))
-}
+// const setImages = (location, array) => {
+//   return {
+//     type: ADD_IMAGE,
+//     array,
+//     location,
+//   };
+// };
 
 
+// export const getEmbeddedCode = (location, position = 0) => async dispatch => {
+//   const response = await csrfFetch(`api/embedded/${location}`)
+//   const data = await response.json()
+//   dispatch(setEmbeddedCode(location, data.array))
+//   /*
+//     RESPONSE SHOULD RETURN AN ARRAY OF ALL THE EMBEDDED CODE FROM THAT LOCATION
+//     RESPONSE SHOULD BE AN ARRAY OF EMBEDDED CODE WITH THEIR POSITION AND LOCATION
 
-export const removeImage = (id, location) => async dispatch => {
-  const response = await csrfFetch (`api/images/${id}`, {
-    method: 'DELETE'
-  })
-  const data = await response.json()
-  dispatch(setImages(location, data.array))
+//   */
+// }
 
-}
+// export const createUpdateCode = (location, position = 0) => async dispatch => {
+//   const response = await csrfFetch(`api/embedded/${location}`)
+//   const data = await response.json()
 
-export const uploadImage = (image, location) => async dispatch => {
-  const formData = new FormData()
-
-  if(image){
-    formData.append('image', image)
-  }
-  formData.append('location', location)
-
-    const response = await csrfFetch('/api/images', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      body: formData, location
-    });
-    const data = await response.json();
-
-    dispatch(setImages(location, data.array));
-    // return response;
-  };
+//   dispatch(setEmbeddedCode(location, data.array))
+// }
 
 
 
+// export const removeImage = (id, location) => async dispatch => {
+//   const response = await csrfFetch (`api/images/${id}`, {
+//     method: 'DELETE'
+//   })
+//   const data = await response.json()
+//   dispatch(setImages(location, data.array))
 
-const embeddedReducer = (state = {}, action) => {
-  switch (action.type) {
-    case ADD_IMAGE:
-      return {
-        ...state,
-        [action.location]: action.array,
+// }
 
-      }
-    default:
-      return state;
-  }
-};
+// export const uploadImage = (image, location) => async dispatch => {
+//   const formData = new FormData()
 
-export default embeddedReducer;
+//   if(image){
+//     formData.append('image', image)
+//   }
+//   formData.append('location', location)
+
+//     const response = await csrfFetch('/api/images', {
+//       method: 'POST',
+//       headers: {
+//         "Content-Type": "multipart/form-data",
+//       },
+//       body: formData, location
+//     });
+//     const data = await response.json();
+
+//     dispatch(setImages(location, data.array));
+//     // return response;
+//   };
+
+
+
+
+// const embeddedReducer = (state = {}, action) => {
+//   switch (action.type) {
+//     case ADD_IMAGE:
+//       return {
+//         ...state,
+//         [action.location]: action.array,
+
+//       }
+//     default:
+//       return state;
+//   }
+// };
+
+// export default embeddedReducer;
