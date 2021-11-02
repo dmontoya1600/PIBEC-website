f2aec9a69770204387ba784d6c040ebbfd6016c9
dmontoya1600 <78572569+dmontoya1600@users.noreply.github.com>
2021-11-02 14:38:59 -0500

    adding styling to check for screen size

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -6,6 +6,7 @@ import SlideShow from './SlideShow';
 import EmbeddedPost from './EmbeddedPost';
 import './HomePage.css'
 import { getImages } from '../../store/images';
+import {isInViewport} from '../../Help_functions'
 
 
 function HomePage() {
@@ -16,6 +17,11 @@ function HomePage() {
   useEffect(() => {
 
   }, [])
+  function vpResult(ele){
+    if(isInViewport(ele)){
+      return 'scroll-transition-fade'
+    }
+  }
 
   return (
     <div className='home__page'>
@@ -39,14 +45,16 @@ function HomePage() {
           </div>
             <EmbeddedPost location={location}/>
             <div id='about_us' className='about__us__home'>
-              About Us
-              <p>
+              <p className={"about__us__text " + vpResult} >About Us</p>
+              <p className='about__believe__title'>Lo que creemos.</p>
+              <p className='about__believe__content'>
                 Creemos en el Padre, Hijo, y El Espíritu Santo. Se predica de la Santa Biblia Reyna Valera Gomez. La salvación viene por El arrepentimiento de sus pecados y recibiendo a Jesucristo como su Señor y Salvador!
               </p>
+              <p className='about__location'>Donde estamos.</p>
+              <div id='google__map' dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3587.886786570354!2d-97.43290918497543!3d25.938941883556623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x866fbe0425939131%3A0xb4534ad760db747a!2s2000%20N%20Minnesota%20Ave%2C%20Brownsville%2C%20TX%2078521!5e0!3m2!1sen!2sus!4v1635742369010!5m2!1sen!2sus" width="550" height="350" style="border:0;" allowfullscreen="" loading="lazy" />`}} />
             </div>
             <div className='inner__stuff'>This thing is going to have a lot of  stuff</div>
             <div className='rando__stuff'>RANDOM STUFF</div>
-            <div id='google__map' dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3587.886786570354!2d-97.43290918497543!3d25.938941883556623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x866fbe0425939131%3A0xb4534ad760db747a!2s2000%20N%20Minnesota%20Ave%2C%20Brownsville%2C%20TX%2078521!5e0!3m2!1sen!2sus!4v1635742369010!5m2!1sen!2sus" width="550" height="350" style="border:0;" allowfullscreen="" loading="lazy" />`}} />
         </div>
     </div>
   );
