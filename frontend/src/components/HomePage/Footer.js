177b164c8646a6aa296a365bfaa7b07fcb5d0fec
dmontoya1600 <78572569+dmontoya1600@users.noreply.github.com>
2021-11-06 14:43:38 -0500

    fixed some bugs in the contact form. now values reset onSubmit

--- frontend/src/components/HomePage/Footer.js
+++ frontend/src/components/HomePage/Footer.js
@@ -12,24 +12,26 @@ function Footer({ isLoaded }){
   const [embeddedCode, setEmbeddedCode] = useState(null)
   const ref = useRef()
   const isVisible = useOnScreen(ref)
-  const [emailValue, setEmail] = useState(null)
-  const [messageValue, setMessage] = useState(null)
+  const [emailValue, setEmail] = useState('')
+  const [messageValue, setMessage] = useState('')
 
-  function submitContactForm(e){
+  async function submitContactForm(e){
       e.preventDefault()
     const submitValue = {
         email: emailValue,
         message: messageValue
     }
-    dispatch(submitForm(submitValue))
+    await dispatch(submitForm(submitValue))
+    setEmail('')
+    setMessage('')
   }
   return (
     <div id='footer' className='homepage__footer'>
         <form onSubmit={(e) => submitContactForm(e)} className={"footer__contact__us scroll-transition-fade " +(useOnScreen(ref) ? 'nothing' : 'below-viewport')}  ref={ref}>
             <p className='footer__message__1' >Do you have a question or a prayer request?</p>
             <p className='footer__message__2' >Contact Us.</p>
-            <input onChange={(e) => setEmail(e.target.value)} className='contact__email' required type='email' placeholder='Email'/>
-            <textarea onChange={(e) => setMessage(e.target.value)} className='contact__message' required placeholder='Message'/>
+            <input value={emailValue} onChange={(e) => setEmail(e.target.value)} className='contact__email' required type='email' placeholder='Email'/>
+            <textarea value={messageValue} onChange={(e) => setMessage(e.target.value)} className='contact__message' required placeholder='Message'/>
             <input type='submit' className='contact__submit'/>
         </form>
         <div className={"footer__tag scroll-transition-fade " +(useOnScreen(ref) ? 'nothing' : 'below-viewport')} >
