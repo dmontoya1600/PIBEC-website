934b2409a2d93013fdb9218b27e96b1dff7e7368
dmontoya1600 <78572569+dmontoya1600@users.noreply.github.com>
2021-12-03 20:27:49 -0600

    completed calendar UI to include next month days too

---
+++ frontend/src/components/Calendar/calendarFunction.js
@@ -0,0 +1,62 @@
+
+let currentDate = new Date()
+let month = currentDate.getMonth()
+let year = currentDate.getFullYear()
+let dayOfMonth = currentDate.getDate()
+let daysInMonth = new Date(year, month +1, 0).getDate()
+let monthObj={}
+function daysIntoYear(date){
+  return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+}
+
+for(let i = 1; i <= daysInMonth; i++){
+  let thisDate = new Date(year, month, i);
+  let dayOfWeek = thisDate.getDay()
+  if(i === 1 && dayOfWeek > 0){
+      let lastMonth;
+      let yearOfMonth = year
+      if(month > 1){
+          lastMonth = month - 1
+      } else{
+          lastMonth = 11
+          yearOfMonth = year - 1
+      }
+      let daysInLastMonth = new Date(yearOfMonth, lastMonth, 0).getDate()
+      for(let j = 0; j < dayOfWeek; j++){
+          let subDay = dayOfWeek - j
+          let lastMonthDay = new Date(yearOfMonth, lastMonth, daysInLastMonth - subDay)
+          let dayOfMonth = new Date(yearOfMonth, lastMonth, daysInLastMonth - subDay).getDate()
+          let dayOfYear = daysIntoYear(lastMonthDay)
+          monthObj[dayOfYear] = {dayOfMonth, monthDay: lastMonthDay, weekDay: j, month: lastMonth +1, yearOfMonth}
+          console.log('INSIDE MONTH', monthObj)
+      }
+  }
+  let dayOfYear = daysIntoYear(thisDate)
+  monthObj[dayOfYear] = {dayOfMonth: i,monthDay: thisDate, weekDay: dayOfWeek, month: month +1, year}
+  if(i === daysInMonth && dayOfWeek < 6){
+    let nextDays = 1;
+    let nextMonth;
+    let yearOfMonth = year
+    let newYear = false
+    if(month === 11){
+        nextMonth = 1
+        yearOfMonth = year + 1
+        newYear = true
+    } else{
+        nextMonth = month + 1
+    }
+    for(let j = dayOfWeek + 1; j <=6 ; j++){
+        let nextMonthDay = new Date(yearOfMonth, nextMonth, nextDays)
+        let nextDayOfYear = daysIntoYear(nextMonthDay)
+        if(newYear === true){
+            nextDayOfYear+=365
+        }
+
+        monthObj[nextDayOfYear] = {dayOfMonth: nextDays, monthDay: nextMonthDay, weekDay: j, month: nextMonth +1, yearOfMonth}
+        nextDays++
+
+    }
+  }
+}
+
+export {monthObj};
--- frontend/src/components/Calendar/index.js
+++ frontend/src/components/Calendar/index.js
@@ -1,54 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './Calendar.css'
-
-let currentDate = new Date()
-let month = currentDate.getMonth()
-let year = currentDate.getFullYear()
-let dayOfMonth = currentDate.getDate()
-let daysInMonth = new Date(year, month +1, 0).getDate()
-let monthObj={}
-function daysIntoYear(date){
-  return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
-}
-
-for(let i = 1; i <= daysInMonth; i++){
-  let thisDate = new Date(year, month, i);
-  let dayOfWeek = thisDate.getDay()
-  if(i === 1 && dayOfWeek > 0){
-      let lastMonth;
-
-      if(month > 1){
-          lastMonth = month - 1
-      } else{
-          lastMonth = 12
-      }
-      let daysInLastMonth = new Date(year, lastMonth, 0).getDate()
-
-      for(let j = 0; j < dayOfWeek; j++){
-          let subDay = dayOfWeek - j
-          let lastMonthDay = new Date(year, lastMonth, daysInLastMonth - subDay)
-          let dayOfMonth = new Date(year, lastMonth, daysInLastMonth - subDay).getDate()
-          let dayOfYear = daysIntoYear(lastMonthDay)
-          monthObj[dayOfYear] = {dayOfMonth, monthDay: lastMonthDay, weekDay: j, month: lastMonth +1, year}
-          console.log('INSIDE MONTH', monthObj)
-      }
-  }
-  let dayOfYear = daysIntoYear(thisDate)
-  monthObj[dayOfYear] = {dayOfMonth: i,monthDay: thisDate, weekDay: dayOfWeek, month: month +1, year}
-
-}
-
+import {monthObj} from './calendarFunction'
 
 function Calendar(){
   const dispatch = useDispatch();
-  const [month, setMonth] = useState(monthObj)
+  const [calendar, setCalendar] = useState(monthObj)
   console.log('month', monthObj)
 
   return (
     <div className='calendar__component'>
         <div className='calendar__page'>
-            {Object.keys(month).map(day => {
+            {Object.keys(calendar).map(day => {
                 let date = monthObj[day]
                 return(
                     <div key={day} className={`calendar__day ${date.dayOfMonth === new Date().getDate() ? 'today__date' : ''}`}>
